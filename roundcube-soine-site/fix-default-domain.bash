eca6c1cc0bd595d283174b2ad0c8957f1d79c2cb
yude <i@yude.jp>
2023-08-20 12:30:41 +0000

    Update

--- roundcube-soine-site/fix-default-domain.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker compose up -d
-sleep 3
-docker compose exec -it app sh -c "echo '\$config[\"product_name\"] = \"soine.site Web メール\";' >> /var/www/html/config/config.inc.php"
-docker compose exec -it app sh -c "echo '\$config[\"mail_domain\"] = \"soine.site\";' >> /var/www/html/config/config.inc.php"
-docker compose exec -it app sh -c "grep -v "mail_domain" /var/www/html/config/defaults.inc.php > tmpfile && mv tmpfile /var/www/html/config/defaults.inc.php"
-docker compose restart app
